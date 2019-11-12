7edd39232d1d6241176453af12e4bc3ba6264acf
PedroDelgado <pdelgadof1@gmail.com>
2019-11-12 14:12:18 -0600

    se agregaron

---
+++ application/controllers/Inicio.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Inicio extends  CI_Controller
+{
+	
+	public function __contructor()
+	{
+		parent:: Contoller();
+	}
+
+	public function index()
+	{
+		$this->load->view("vistas/head");
+	}
+}
+?>
